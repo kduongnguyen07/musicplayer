e9432e5fdf618e9c89035fcefa65ad5b59e8c3ec
kduongnguyen07 <100816706+kduongnguyen07@users.noreply.github.com>
2022-08-14 10:43:38 +0700

    thêm 1 tỷ bài hát

--- js/music-list.js
+++ js/music-list.js
@@ -18,7 +18,7 @@ let allMusic = [
     src: "music-3"
   },
   {
-    name: "Tình đầu",
+    name: "Tình Đầu",
     artist: "Tăng Duy Tân",
     img: "music-4",
     src: "music-4"
@@ -36,19 +36,19 @@ let allMusic = [
     src: "music-6"
   },
   {
-    name: "Kẻ theo đuổi ánh sáng",
+    name: "Kẻ Theo Đuổi Ánh Sáng",
     artist: "Huy Vạc x Tiến Nguyễn ",
     img: "music-7",
     src: "music-7"
   },
   {
-    name: "Đường tôi chở em về",
+    name: "Đường Tôi Chở Em Về",
     artist: "buitruonglinh ",
     img: "music-8",
     src: "music-8"
   },
   {
-    name: "Dù cho mai về sau",
+    name: "Dù Cho Mai Về Sau",
     artist: "buitruonglinh ",
     img: "music-9",
     src: "music-9"
@@ -60,9 +60,75 @@ let allMusic = [
     src: "music-10"
   },
   {
-    name: "Bên trên tầng lầu",
+    name: "Bên Trên Tầng Lầu",
     artist: "Tăng Duy Tân",
     img: "music-11",
     src: "music-11"
+  },
+  {
+    name: "Vẫn Nhớ By Jimmii Nguyễn Cover By Soobin ",
+    artist: "Soobin Hoàng Sơn",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Trú Mưa",
+    artist: "HKT",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Đôi Mắt",
+    artist: "Wanbi Tuấn Anh",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Thanh Xuân",
+    artist: "Da LAB",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Hãy Trao Cho Anh",
+    artist: "Sơn Tùng M-TP ft Snoop Dogg",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Lạc Trôi",
+    artist: "Sơn Tùng M-TP",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Chúng Ta Không Thuộc Về nhau",
+    artist: "Sơn Tùng M-TP",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Nơi Này Có Anh",
+    artist: "Sơn Tùng M-TP",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Chạy Ngay Đi",
+    artist: "Sơn Tùng M-TP",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name:"Ghen",
+    artist:"Khắc Hưng x Min x Erik",
+    img:"music-21",
+    src:"music-21"
+  },
+  {
+    name:"Em Không Sai Chúng Ta Sai",
+    artist:"Erik",
+    img:"music-22",
+    src:"music-22"
   },
 ];
